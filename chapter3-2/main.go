7d93e89c0bcac077d853f944e4ff2682d0ca28ce
puyolito <daruna.th@tnis.com>
2018-02-07 15:47:22 +0700

    Add main.go file

--- chapter3-2/main.go
+++ chapter3-2/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("=====String=====")
-	backticks := 'hello world!,
-today's good day.'
+	backticks := `hello world!,
+today's good day.`
 	fmt.Println(backticks)
 
 	doubleQuotes := "hello world!,\ntoday's good day."
